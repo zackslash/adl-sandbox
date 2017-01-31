72c3580bca588632f35288e25b8945836c0d0e0b
Luke Hines <lukehines@protonmail.com>
2017-01-31 09:28:26 +0000

    Adds initial ADL sandbox code

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
+	"time"
 
+	"github.com/cubex/potens-go/adl"
 	"github.com/cubex/potens-go/application"
 	"github.com/cubex/proto-go/applications"
 	zipkin "github.com/openzipkin/zipkin-go-opentracing"
@@ -61,6 +64,71 @@ func main() {
 	if err != nil {
 		app.Logger.Fatal(err.Error())
 	}
+
+	// --- FDL TEST LOOP START ---
+	for {
+		ent1 := app.ADL("THIS-IS-A-FID")
+		err = ent1.Read(adl.Counter("propX"))
+		countA := ent1.GetCounter("propX")
+		testStrStart := "this is data!"
+
+		if countA <= 0 {
+			// Write
+			ent1.Write("propX", testStrStart)
+		} else {
+			str := testStrStart
+			for i := 0; i < countA; i++ {
+				str += "!"
+			}
+			ent1.Write("propX", str)
+		}
+
+		ent1.WriteMeta("propX", "this is meta")
+
+		ent1.AddSetItem("propX", "test1")
+		ent1.AddSetItem("propX", "test2")
+		ent1.AddSetItem("propX", "test3")
+		//ent1.RemoveSetItem("propX", "test2")
+
+		// Test List Add
+		testListName := "TESTLIST"
+		ent1.AddListItem(testListName, "1", "ONE")
+		ent1.AddListItem(testListName, "2", "TWO")
+		ent1.AddListItem(testListName, "3", "THREE")
+		ent1.AddListItem(testListName, "4", "FOUR")
+
+		ent1.IncrementCounter("propX")
+		ent1.Write("propY", "This is Y data")
+		ent1.Commit()
+
+		// TODO: retrieve items with prefix
+		ent1.Read(adl.PropertiesWithPrefix("prop"), adl.Meta("propX"), adl.Counter("propX"), adl.Set("propX"), adl.ListRange(testListName, "1", "", 0))
+		dataA := ent1.Get("propX")
+		countA = ent1.GetCounter("propX")
+		data3 := ent1.GetSet("propX")
+		data4 := ent1.Get("propY")
+		meta1 := ent1.GetMeta("propX")
+		list := ent1.GetList(testListName)
+
+		fmt.Printf("\nItem:%s-%s-%s\n", dataA, data4, meta1)
+		fmt.Printf("Counter:%d\n", countA)
+
+		for _, d := range data3 {
+			fmt.Printf("SET-ITEM:%s\n", d)
+		}
+
+		for _, d := range list {
+			fmt.Printf("LIST-ITEM:%s - %s\n", d.Key, d.Value)
+		}
+
+		if len(list) == 0 {
+			fmt.Printf("No list items returned\n")
+		}
+
+		t := time.Second * 5
+		time.Sleep(t)
+	}
+	// --- FDL TEST LOOP END ---
 
 	//When interrupted, take the app offline with discovery first
 	c := make(chan os.Signal, 1)
