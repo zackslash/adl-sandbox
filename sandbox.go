72ace61c203a0cbfff98b8eaadec9b16e6d19ef3
Luke Hines <lukehines@protonmail.com>
2017-02-02 10:59:44 +0000

    Adds test for lists

--- sandbox.go
+++ sandbox.go
@@ -9,10 +9,6 @@ import (
 // runSandbox is executed every 5 seconds (ADL sandbox code goes in this method)
 func runSandbox(count int) {
 	ent1 := app.ADL("THIS-IS-A-FID")
-	/*err := ent1.Read(adl.Counter("propX"))
-	if err != nil {
-		fmt.Printf(err.Error())
-	}*/
 
 	// Test counters
 	testStrStart := "this is data!"
@@ -40,11 +36,9 @@ func runSandbox(count int) {
 	ent1.Commit()
 
 	//adl.Counter("propX"),
-	//adl.PropertiesWithPrefix("prop"),
-	//adl.ListRange(testListName, "1", "", 0)
 	//adl.Property("propX")
 
-	ent1.Read(adl.PropertiesWithPrefix("prop"), adl.Meta("propX"), adl.Set("propX"), adl.ListItem(testListName, "1"))
+	ent1.Read(adl.PropertiesWithPrefix("prop"), adl.Meta("propX"), adl.Set("propX"), adl.ListRange(testListName, "0", "4", 10))
 	dataA := ent1.Get("propX")
 	//countA = ent1.GetCounter("propX")
 	data3 := ent1.GetSet("propX")
@@ -53,7 +47,6 @@ func runSandbox(count int) {
 	list := ent1.GetList(testListName)
 
 	fmt.Printf("\nItem:%s-%s-%s\n", dataA, data4, meta1)
-	//fmt.Printf("Counter:%d\n", countA)
 
 	for _, d := range data3 {
 		fmt.Printf("SET-ITEM:%s\n", d)
